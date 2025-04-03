cb996421176205f7685a8689c405297436889e06
Sean Smith <sasmith922@icloud.com>
2025-04-02 21:32:30 -0700

    uncomment avl

--- bst-test.cpp
+++ bst-test.cpp
@@ -9,42 +9,42 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     cout << "start" << endl;
-    // Binary Search Tree tests
-    BinarySearchTree<char,int> bt;
-    cout << "init" << endl;
-    bt.insert(std::make_pair('a',1));
-    bt.insert(std::make_pair('b',2));
+    // // Binary Search Tree tests
+    // BinarySearchTree<char,int> bt;
+    // cout << "init" << endl;
+    // bt.insert(std::make_pair('a',1));
+    // bt.insert(std::make_pair('b',2));
     
-    cout << "Binary Search Tree contents:" << endl;
-    for(BinarySearchTree<char,int>::iterator it = bt.begin(); it != bt.end(); ++it) {
-        cout << it->first << " " << it->second << endl;
-    }
-    if(bt.find('b') != bt.end()) {
-        cout << "Found b" << endl;
-    }
-    else {
-        cout << "Did not find b" << endl;
-    }
-    cout << "Erasing b" << endl;
-    bt.remove('b');
-
-    // // AVL Tree Tests
-    // AVLTree<char,int> at;
-    // at.insert(std::make_pair('a',1));
-    // at.insert(std::make_pair('b',2));
-
-    // cout << "\nAVLTree contents:" << endl;
-    // for(AVLTree<char,int>::iterator it = at.begin(); it != at.end(); ++it) {
+    // cout << "Binary Search Tree contents:" << endl;
+    // for(BinarySearchTree<char,int>::iterator it = bt.begin(); it != bt.end(); ++it) {
     //     cout << it->first << " " << it->second << endl;
     // }
-    // if(at.find('b') != at.end()) {
+    // if(bt.find('b') != bt.end()) {
     //     cout << "Found b" << endl;
     // }
     // else {
     //     cout << "Did not find b" << endl;
     // }
     // cout << "Erasing b" << endl;
-    // at.remove('b');
+    // bt.remove('b');
 
-    // return 0;
+    // AVL Tree Tests
+    AVLTree<char,int> at;
+    at.insert(std::make_pair('a',1));
+    at.insert(std::make_pair('b',2));
+
+    cout << "\nAVLTree contents:" << endl;
+    for(AVLTree<char,int>::iterator it = at.begin(); it != at.end(); ++it) {
+        cout << it->first << " " << it->second << endl;
+    }
+    if(at.find('b') != at.end()) {
+        cout << "Found b" << endl;
+    }
+    else {
+        cout << "Did not find b" << endl;
+    }
+    cout << "Erasing b" << endl;
+    at.remove('b');
+
+    return 0;
 }
